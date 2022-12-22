f6a8ca77aec173b8685a22646a6a43fd983adc19
mintpancake <bouncy.lu@hotmail.com>
2022-12-22 14:13:53 +0800

    Add echo dataset

--- dataset.py
+++ dataset.py
@@ -1,15 +1,36 @@
 import os
-import torch
+import csv
+import nrrd
 from torch.utils.data import Dataset
 
 
 class EchoData(Dataset):
     def __init__(self, meta_dir) -> None:
         super().__init__()
-        self.size = len(os.listdir(meta_dir))
+        self.meta_dir = meta_dir
+        self.csv_names = os.listdir(meta_dir)
+        self.size = len(self.csv_names)
+        self.metas = self.load(self.meta_dir, self.csv_names)
 
     def __len__(self):
-        pass
+        return self.size
 
     def __getitem__(self, index):
-        return self.size
+        meta = self.metas[index]
+        echo_data = nrrd.read(meta[0][2])[0]
+        truth_data = nrrd.read(meta[0][3])[0]
+        structs = [row[1] for row in meta]
+        return (echo_data, truth_data, structs)
+
+    def load(self, meta_dir, csv_names):
+        metas = []
+        for csv_name in csv_names:
+            csv_reader = csv.reader(
+                open(os.path.join(meta_dir, csv_name), 'r'))
+            meta = []
+            for row in csv_reader:
+                if csv_reader.line_num == 1:
+                    continue
+                meta.append([int(row[0]), int(row[1]), row[2], row[3]])
+            metas.append(meta)
+        return metas
