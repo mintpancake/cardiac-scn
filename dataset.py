959234c7f9d26895537e4d561130dd8ab95a9cfa
mintpancake <bouncy.lu@hotmail.com>
2023-01-07 22:23:02 +0800

    fix aug

--- dataset.py
+++ dataset.py
@@ -24,17 +24,19 @@ class EchoData(Dataset):
 
     def __getitem__(self, index):
         meta = self.metas[index]
-        echo_data = nrrd.read(meta[0][2])[0]
-        truth_data = nrrd.read(meta[0][3])[0]
+        echo_data = nrrd.read(meta[0][2])[0]  # np.uint8
+        truth_data = nrrd.read(meta[0][3])[0]  # np.float64
 
         if self.augmentation:
             zoom = np.random.uniform(0.9, 1.1)
             shift = np.random.randint(-5, 5, size=3)
             angles = (np.random.uniform(-15, 15),
-                      np.random.uniform(-5, 5),
-                      np.random.uniform(-5, 5))
+                      np.random.uniform(-10, 10),
+                      np.random.uniform(-10, 10))
             echo_data, truth_data = self.augment(
                 echo_data, truth_data, zoom, shift, angles)
+
+        echo_data = echo_data.astype(np.float64)  # np.uint8 -> np.float64
 
         if self.norm_echo:
             echo_data = self.normalize_echo(echo_data)
@@ -47,8 +49,8 @@ class EchoData(Dataset):
             add = np.random.uniform(-0.25, 0.25)
             echo_data = self.adjust_intensity(echo_data, multiply, add)
 
-        echo_data = torch.from_numpy(echo_data).float()
-        echo_data = torch.unsqueeze(echo_data, dim=0)
+        echo_data = torch.from_numpy(
+            echo_data).float().unsqueeze(echo_data, dim=0)
         truth_data = torch.from_numpy(truth_data).float()
         structs = torch.IntTensor(sorted([row[1] for row in meta]))
 
@@ -58,19 +60,29 @@ class EchoData(Dataset):
         size = echo_data.shape
 
         if zoom < 1.0:
-            echo_data = self.scale(echo_data, zoom, size)
-        echo_data = self.rotate_and_translate(echo_data, angles, shift, size)
-        if zoom > 1.0:
-            echo_data = self.scale(echo_data, zoom, size)
+            echo_data = self.scale(echo_data, zoom)
+            echo_data = self.fit_size(echo_data, size)
+            echo_data = self.rotate_and_translate(echo_data, angles, shift)
+            echo_data = self.fit_size(echo_data, size)
+        else:
+            echo_data = self.rotate_and_translate(echo_data, angles, shift)
+            echo_data = self.scale(echo_data, zoom)
+            echo_data = self.fit_size(echo_data, size)
 
-        transformed_truth_data = np.zeros_like(truth_data)
+        transformed_truth_data = np.zeros_like(
+            truth_data, dtype=truth_data.dtype)
         for channel, channel_data in enumerate(truth_data):
             if zoom < 1.0:
-                channel_data = self.scale(channel_data, zoom, size)
-            channel_data = self.rotate_and_translate(
-                channel_data, angles, shift, size)
-            if zoom > 1.0:
-                channel_data = self.scale(channel_data, zoom, size)
+                channel_data = self.scale(channel_data, zoom)
+                channel_data = self.fit_size(channel_data, size)
+                channel_data = self.rotate_and_translate(
+                    channel_data, angles, shift)
+                channel_data = self.fit_size(channel_data, size)
+            else:
+                channel_data = self.rotate_and_translate(
+                    channel_data, angles, shift)
+                channel_data = self.scale(channel_data, zoom)
+                channel_data = self.fit_size(channel_data, size)
             transformed_truth_data[channel] = channel_data
 
         return echo_data, transformed_truth_data
@@ -78,24 +90,19 @@ class EchoData(Dataset):
     def fit_size(self, data, size):
         data_size = np.array(data.shape)
         size = np.array(size)
-        # crop larger dimensions
-        for i in range(3):
-            if data_size[i] > size[i]:
-                start = (data_size[i]-size[i])//2
-                end = start+size[i]
-                data = data.take([start, end], axis=i)
         # pad smaller dimensions
-        data_size = np.array(data.shape)
-        start = (size-data_size)//2
+        padded_size = np.maximum(data_size, size)
+        padded_data = np.zeros(padded_size, dtype=data.dtype)
+        start = (padded_size-data_size)//2
         end = start+data_size
-        fitted_data = np.zeros(size)
-        fitted_data[start[0]:end[0], start[1]:end[1], start[2]:end[2]] = data
-        return fitted_data
+        padded_data[start[0]:end[0], start[1]:end[1], start[2]:end[2]] = data
+        # crop larger dimensions
+        start = (padded_size-size)//2
+        end = start+size
+        return padded_data[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
 
-    def rotate_and_translate(self, data, angles, shift, size):
-        data = self.rotate(data, angles)
-        data = self.translate(data, shift)
-        return self.fit_size(data, size)
+    def rotate_and_translate(self, data, angles, shift):
+        return self.translate(self.rotate(data, angles), shift)
 
     def translate(self, data, shift):
         return ndimage.shift(data, shift, cval=0.0)
@@ -109,14 +116,11 @@ class EchoData(Dataset):
             data, angles[2], axes=(0, 2), reshape=True, cval=0.0)
         return data
 
-    def scale(self, data, zoom, size):
-        data = ndimage.zoom(data, zoom, cval=0.0)
-        return self.fit_size(data, size)
+    def scale(self, data, zoom):
+        return ndimage.zoom(data, zoom, cval=0.0)
 
     def adjust_intensity(self, data, multiply, add):
-        data *= multiply
-        data += add
-        return data
+        return (data*multiply)+add
 
     def normalize_echo(self, data):
         max_echo = data.max()
